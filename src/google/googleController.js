edaa72d20183786670d572ef29e940fbde8de8b0
hachi-nico <nicokun48@gmail.com>
2022-05-03 11:56:48 +0700

    cleaning

--- src/google/googleController.js
+++ src/google/googleController.js
@@ -33,13 +33,6 @@ const getAuth = (req, res) => {
   }
 };
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
 const authorize = (credentials, callback) => {
   const clientSecret = credentials.web.client_secret;
   const clientId = credentials.web.client_id;
@@ -57,14 +50,6 @@ const authorize = (credentials, callback) => {
   });
 };
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
 function getNewToken(oauth2Client, callback) {
   const authUrl = oauth2Client.generateAuthUrl({
     access_type: "offline",
@@ -89,11 +74,6 @@ function getNewToken(oauth2Client, callback) {
   });
 }
 
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
 function storeToken(token) {
   try {
     fs.mkdirSync(TOKEN_DIR);
@@ -108,11 +88,6 @@ function storeToken(token) {
   });
 }
 
-/**
- * Lists the names and IDs of up to 10 files.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
 function getChannel(auth) {
   const service = google.youtube("v3");
   service.channels.list(
