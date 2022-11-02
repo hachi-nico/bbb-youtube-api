3120c96e28440459966ab864cfd2fd715aace3f3
hachi-nico <nicokun48@gmail.com>
2022-11-02 19:38:12 +0700

    testing

--- src/controller/bbbCallbackController.js
+++ src/controller/bbbCallbackController.js
@@ -1,5 +1,7 @@
+const jwt = require("jsonwebtoken");
+
 const listenRecordingReady = (req, res) => {
-  console.log(req.body);
+  console.log(jwt.decode(req.body.signed_parameters));
   console.log("webhook recording ready called");
 
   return res.json({ msg: "yay" });
