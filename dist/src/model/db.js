acf919aedc8b3102cb0027895044abcefd43b6eb
hachi-nico <nicokun48@gmail.com>
2022-12-29 13:48:10 +0700

    babe

--- dist/src/model/db.js
+++
@@ -1,9 +0,0 @@
-const Client = require("pg").Pool;
-const pool = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT
-});
-module.exports = pool;
